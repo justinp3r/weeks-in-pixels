4e2e14a8b52ef347c181e849b5e6362a890de1a2
Justin <justinperrone1998@gmail.com>
2025-01-30 19:34:04 +0100

    updated vite.config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/weeks-in-pixels/",
   server: {
     port: 5173,  // Setze den Port 5173 für die lokale Entwicklung im Container
     host: '0.0.0.0',  // Wichtige Einstellung: Der Server hört auf allen Netzwerk-Schnittstellen
